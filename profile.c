df5d705e5efc523059304d8aa12072e052a0949d
BenG49 <64862590+BenG49@users.noreply.github.com>
2023-12-04 14:52:34 -0500

    Start motion profile

---
+++ profile.c
@@ -0,0 +1,9 @@
+// https://github.com/wpilibsuite/allwpilib/blob/main/wpimath/src/main/java/edu/wpi/first/math/trajectory/TrapezoidProfile.java
+// https://docs.wpilib.org/en/stable/docs/software/advanced-controls/controllers/trapezoidal-profiles.html
+// https://b2600047.smushcdn.com/2600047/wp-content/uploads/2019/10/Motion-Profile-Feature.jpg?lossy=1&strip=1&webp=0
+
+float MAX_ACCEL, MAX_VEL;
+
+typedef
+
+float getSetpoint(float *outputPosition, float *outputVelocity, float currentPosition, float currentVelocity, float )
